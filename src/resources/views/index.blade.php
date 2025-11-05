0eef211472cec887d1ba8f6f4776b9a0c6c19386
Gaku Ishikawa <amkj1725411.ms364@gmail.com>
2025-11-06 07:01:26 +0900

    1日目

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>フォーム入力ページ</title>
+    
+    </head>
+
+
+    <body>
+        
+    </body>
+
+</html>
